3e561f6006ccede7fa7865c4ec60eca12a66000f
Bassem_Fullstack <bassemali102345@gmail.com>
2026-08-03 16:54:47 +0300

    ssssssssssssssssss

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,14 +14,16 @@ const verifyToken = asyncHandler ( async ( req , res , next) => {
 
  // احنا يا معلم بنبعت اكسيس توكين مش ريفريش توكين عشان اكسيس توكين لو اتهكر من الهكر هيكون لية وقت محدد وهو ربع ساعة وساعتها مش هيعرف يعمل حاجة بعد كدة في الموقع لكن ريفريش توكين بيكون متخزن على مدي بعيد وقت اطول يومين اسبوع او 128 يوم ايا كان احنا بنستخدم ريفريش توكين عشان الامان وعشان نخزنوة في داتا بيز ونبعت توكين وهمي  اسمة اكسيس توكين متاح لمدة ربع ساعة نوعا ما من الامان عشان نتجنب الهكر او اختراق موقع وكمان هكر صعب يخترق اكسيس توكين بيكون متخزن في فروند اند داخل متغير يوس ستيت و دة ميموري مش زاي لوكيل ستوريج يكون متخزن واضح كدة وباين على متصفح لكن هكر صعب يوصل لمتغير اكسيس توكين وحتي لو وصلوة هيبقي ربع ساعة مش هيلحق يعمل حاجة في موقع ولا هيلحق يخترقوة فصعب جدا توصل لاكسيس توكين لان هو متخزن داخل يوسيت ويوستيت دة متغير بتتخزن فية قيم بتاعتة داخل ميموري فصعب يوصل لكود بتاعك عشان يجيب اكسيس توكين ولو وصل صعب يعمل اي حاجة لان اكسيس توكين مدتة ربع ساعة فقط 
 
-const accessToken = req.headers.authorization.replace("Bearer " , "").trim()
+const authHeader = req.headers.authorization;
 
+if (!authHeader || !authHeader.startsWith("Bearer ")) {
 
-if(!accessToken) {
-
-return res.status(401).json({ message : "Not authorized, no token provided" })
+  return res.status(401).json({ message: "Not authorized, no token provided" });
 
 }
+
+const accessToken = authHeader.replace("Bearer " , "").trim()
+
 
 
 try{
@@ -47,7 +49,7 @@ catch(err) {
 
 console.log("error verify token" || err.message) 
  
- return res.status(401).json({ message : "Expired or invalid refresh token" })
+ return res.status(401).json({ message : "Expired or invalid access token" })
 
 }
 
